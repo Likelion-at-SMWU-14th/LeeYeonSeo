046d415c0ae64be825cf2139345b57014f44e342
yexnsxo <yslove829@naver.com>
2026-05-12 19:21:26 +0900

    Date 실습 완료

---
+++ frontend/week03/js/date.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+console.log(
+  `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`,
+);
+
+console.log(
+  `${today.getHours()}시 ${today.getMinutes()}분 ${today.getSeconds()}초`,
+);
