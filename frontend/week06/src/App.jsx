5e51f1209d6c20c69d30261203233de6f0433fa2
yexnsxo <yslove829@naver.com>
2026-06-30 17:03:53 +0900

    style: Movie.jsx 스타일 설정

--- frontend/week06/src/App.jsx
+++ frontend/week06/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Info from "./components/Info";
-import DebouncedSearch from "./components/DebouncedSearch";
+import Movie from "./components/Movie";
 
 function App() {
   return (
     <>
-      <Info />
-      <DebouncedSearch />
+      <Movie />
     </>
   );
 }
