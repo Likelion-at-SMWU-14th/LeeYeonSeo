0a8b93caa5928f69b84044d7627abf9984769cb0
yexnsxo <yslove829@naver.com>
2026-07-06 12:25:02 +0900

    feat: 중첩 라우팅, useNavigate 실습

--- frontend/week07/src/App.jsx
+++ frontend/week07/src/App.jsx
@@ -2,14 +2,19 @@ import "./App.css";
 import QuizPage from "./pages/QuizPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ResultPage from "./pages/ResultPage";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Routes>
-          <Route path="/" element={<QuizPage />} />
-          <Route path="/result" element={<ResultPage />} />
+          {/* 부모 Route */}
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<QuizPage />} />
+            <Route path="quiz/:quizId" element={<QuizPage />} />
+            <Route path="/result" element={<ResultPage />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
