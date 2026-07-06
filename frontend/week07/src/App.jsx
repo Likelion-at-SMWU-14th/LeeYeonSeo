2e2ce1710597ab3f64c2b18e2752c401ce7e8c51
yexnsxo <yslove829@naver.com>
2026-07-06 12:12:46 +0900

    feat: React Router 기초실습

--- frontend/week07/src/App.jsx
+++ frontend/week07/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import QuizPage from "./pages/QuizPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ResultPage from "./pages/ResultPage";
 
 function App() {
   return (
-    <>
-      <QuizPage />
-    </>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<QuizPage />} />
+          <Route path="/result" element={<ResultPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
